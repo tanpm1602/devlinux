a2cbf3e250a56a006b78878f600dfbea437b201a
tanpm1602 <tanpm1602@gmail.com>
2025-10-03 00:09:00 +0700

    chore: remove tracked files that are now ignored

--- memory_lab.c
+++ memory_lab.c
@@ -2,7 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-void stack_overflow() {
+void stack_overflow()
+{
+    printf("stack_overflow function is running\n");
     int var[1000];
     var[9] = 1;
     stack_overflow();
@@ -10,44 +12,49 @@ void stack_overflow() {
 
 void memory_leak()
 {
-    for (int i=0; i<10; i++) 
-    {
-        int *ptr = (int*)malloc(sizeof(int)*1000);
-    } 
+    printf("memory_leak function is running\n");
+    int *ptr = (int *)malloc(sizeof(int) * 1000);
 }
 
-void out_of_memory() {
-    while (1) {
-        int *ptr = (int*)malloc(sizeof(int)*1000);
+void out_of_memory()
+{
+    printf("out_of_memory is running\n");
 
-        if (ptr == NULL) {
+    while (1)
+    {
+        int *ptr = (int *)malloc(sizeof(int) * 1000);
+        if (ptr == NULL)
+        {
             printf("Out of memory!\n");
             break;
         }
     }
 }
 
-int main() 
+int main(int argc, char **argv)
 {
-    char command[50];
-    if (fgets(command, sizeof(command), stdin))
-    {
-        printf("Your command: ");
-        if (strcmp(command, "./memory_lab stack_overflow\n") == 0)
-        {
-            printf("%s", "stack_overflow\n");
-            stack_overflow();
-        }
-        else if (strcmp(command, "./memory_lab memory_leak\n") == 0) 
-        {
-            printf("%s", "memory_leak\n");
-            memory_leak();
-        }
-        else if (strcmp(command, "./memory_lab out_of_memory\n") == 0) 
-        {
-            printf("%s", "out_of_memory\n");
-            out_of_memory();
-        }
+    if (argc < 2) {
+        printf("Choose one of the functions you want to run: stack_overflow, out_of_memory, memory_leak\n");
+        return 1;
     }
-}
 
+    const char *cmd = argv[1];
+    if (strcmp(cmd, "stack_overflow") == 0) {
+        printf("Running: stack_overflow\n");
+        stack_overflow(); 
+    }
+    else if (strcmp(cmd, "memory_leak") == 0) {
+        printf("Running: memory_leak\n");
+        memory_leak();
+    }
+    else if (strcmp(cmd, "out_of_memory") == 0) {
+        printf("Running: out_of_memory\n");
+        out_of_memory();
+    }
+    else {
+        fprintf(stderr, "Unknown command: %s\n\n", cmd);
+        return 2;
+    }
+
+    return 0;
+}
